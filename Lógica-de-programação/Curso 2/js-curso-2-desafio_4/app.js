3e574cc5668eb8596f4cc7db203d013ff4f545d8
Jhonnatan Luiz <jhonnatanluiz@yahoo.com.br>
2025-09-18 14:41:14 -0300

    Add initial JavaScript code for fruit, programming languages, and names lists

---
+++ Lógica-de-programação/Curso 2/js-curso-2-desafio_4/app.js
@@ -0,0 +1,44 @@
+//Criando uma lista vazia de frutas
+let listaDeFrutas = []; // Lista vazia para frutas
+
+// Adicionando elementos à lista
+listaDeFrutas.push('Maçã');
+listaDeFrutas.push('Banana');
+listaDeFrutas.push('Laranja');
+listaDeFrutas.push('Uva');
+listaDeFrutas.push('Manga');
+
+// Exibindo a lista no console
+console.log(listaDeFrutas);
+
+//Criando uma lista com linguagens de programação
+
+let linguagemDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python']
+
+
+// Adicionando mais linguagens
+linguagemDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+// Exibindo a lista no console
+console.log(linguagemDeProgramacao);
+
+// Criando uma lista com nomes
+
+let listaDeNomes = ['Ana', 'Bruno', 'Carlos'];
+
+// Exibir no console apenas o primeiro nome da lista
+console.log(listaDeNomes[0]);
+
+// Criando outra lista com nomes
+
+let listaDeNomes2 = ['Carlos', 'Pedro', 'Jhonnatan'];
+
+// Exibir no console apenas o segundo nome da lista
+console.log(listaDeNomes2[1]);
+
+// Criando mais uma lista com nomes
+
+let listaDeNomes3 = ['Ana', 'Bruno', 'Carlos'];
+
+// Exibir no console apenas o terceiro nome da lista
+console.log(listaDeNomes3[2]);
